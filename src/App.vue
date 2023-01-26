e24ca82b2c58f53b02850f38d9c2807e86844654
manuelmenama <manuel.mena090589@gmail.com>
2023-01-26 17:43:19 +0100

    milestone 4

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,37 @@
 <script>
-export default {
 
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      baseUrl: 'http://127.0.0.1:8000/api/',
+      projects: []
+    }
+  },
+  methods:{
+    getApi(){
+      axios.get(this.baseUrl + 'projects')
+        .then(result => {
+          this.projects = result.data.projects;
+          console.log(this.projects);
+        })
+    }
+  },
+  mounted(){
+    this.getApi();
+  }
 }
 </script>
 
 <template>
+  <div class="container">
 
-  <h1>Ciao vue</h1>
+    <h1>Chiamata axios from vue</h1>
+
+
+  </div>
   
 </template>
 
